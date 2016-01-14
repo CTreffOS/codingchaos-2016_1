18050ed85643f312507dd192845b6b4a33d158f6
davidak <git@davidak.de>
2016-01-14 19:54:58 +0100

    Aufgabe 2

---
+++ aufgabe02/davidak/change.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import csv
+
+print("#0.01,0.02,0.05,0.1,0.2,0.5,1,2,5,10,20,50,100,200,500")
+
+def calculate_change(price,money):
+	change = money - price
+	coins = [ 0.01,0.02,0.05,0.1,0.2,0.5,1,2,5,10,20,50,100,200,500 ]
+	result = []
+	for coin in reversed(coins):
+		if change / coin > 1:
+			amount_of_coins = int(change / coin)
+			result.append(amount_of_coins)
+			change -= amount_of_coins
+		else:
+			result.append(0)
+	return list(reversed(result))
+
+with open('amounts.csv', 'r') as f:
+    reader = csv.reader(f)
+    for row in reader:
+    	price, money = row
+    	print(calculate_change(float(price), float(money)))
